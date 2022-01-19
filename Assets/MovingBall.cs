b602213cfab06aaa4361eaec07a0b9b3b056947d
Adrián Jaime Pastor <36241023+AdrianJaime@users.noreply.github.com>
2022-01-19 02:23:23 +0100

    Initial

--- Assets/MovingBall.cs
+++ Assets/MovingBall.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovingBall : MonoBehaviour
 {
@@ -11,13 +12,23 @@ public class MovingBall : MonoBehaviour
     [Range(-1.0f, 1.0f)]
     [SerializeField]
     private float _movementSpeed = 5f;
+    private float mass = 1f;
+    private bool shoot = false;
+    private bool stopped = false;
+    private bool goal = true;
+    private GameObject target;
+    private Vector3 dirVec, originalPosition;
+    private Text rotationVelocityText;
 
-    Vector3 _dir;
+    public float shootSpeed, rotationSpeed, effect, actualRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("BlueTarget");
+        dirVec = Vector3.zero;
+        originalPosition = transform.position;
+        rotationVelocityText = GameObject.Find("Rotation Velocity Text").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -25,18 +36,45 @@ public class MovingBall : MonoBehaviour
     {
         transform.rotation = Quaternion.identity;
 
-        //get the Input from Horizontal axis
-        float horizontalInput = Input.GetAxis("Horizontal");
-        //get the Input from Vertical axis
-        float verticalInput = Input.GetAxis("Vertical");
+        if (shoot && !stopped)
+        {
+            ShootBall();
+        }
+    }
 
-        //update the position
-        transform.position = transform.position + new Vector3(-horizontalInput * _movementSpeed * Time.deltaTime, verticalInput * _movementSpeed * Time.deltaTime, 0);
+    void ShootBall()
+    {
+        Vector3 newPos;
+
+        newPos.x = transform.position.x + 
+        gameObject.transform.Translate(dirVec * shootSpeed * Time.deltaTime);
+
+        actualRotation += effect * shootSpeed * rotationSpeed * Time.deltaTime;
+        rotationVelocityText.text = "Rotation Velocity: " + (effect * shootSpeed * rotationSpeed) + " deg/s";
+        transform.Rotate(Vector3.up, actualRotation, Space.World);
+        Debug.Log("Shot with " + actualRotation);
 
     }
 
+    public void ResetPosition()
+    {
+        dirVec = Vector3.zero;
+        goal = goal == true ? false : true;
+        stopped = false;
+        shoot = false;
+        transform.position = originalPosition;
+    }
     private void OnCollisionEnter(Collision collision)
     {
+
+        if (collision.gameObject.transform.name == "BallRegion" && !goal)
+            stopped = true;
+        else if (goal && !shoot)
+        {
         _myOctopus.NotifyShoot();
+        shoot = true;
+        dirVec = target.transform.position - transform.position;
+        dirVec.Normalize();
+        }
     }
 }
